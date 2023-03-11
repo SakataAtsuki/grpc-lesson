df1305bd8fa6545498f8ad8758f28d4d7dc6b0e1
SakataAtsuki <hhnkkr@icloud.com>
2023-03-11 22:36:42 +0900

    Client Streaming RPC

--- client/main.go
+++ client/main.go
@@ -7,6 +7,8 @@ import (
 	"grpc-lesson/pb"
 	"io"
 	"log"
+	"os"
+	"time"
 )
 
 func main() {
@@ -18,7 +20,8 @@ func main() {
 
 	client := pb.NewFileServiceClient(conn)
 	//callListFiles(client)
-	callDownload(client)
+	//callDownload(client)
+	callUpload(client)
 }
 
 func callListFiles(client pb.FileServiceClient) {
@@ -49,4 +52,46 @@ func callDownload(client pb.FileServiceClient) {
 		log.Printf("Response from Download(byte): %v", res.GetData())
 		log.Printf("Response from Download(string): %v", string(res.GetData()))
 	}
+}
+
+func callUpload(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "/Users/s16252/grpc-lesson/storage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	stream, err := client.Upload(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	buf := make([]byte, 5)
+	for {
+		n, err := file.Read(buf)
+		if n == 0 || err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		req := &pb.UploadRequest{Data: buf[:n]}
+		sendErr := stream.Send(req)
+		if sendErr != nil {
+			log.Fatalln(sendErr)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("received data size: %v", res.GetSize())
 }
